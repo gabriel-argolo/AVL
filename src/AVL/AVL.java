e35dee9237a7f0255a8a1b3e9967d2dc321bc128
FlowOverStack <thundertufik@gmail.com>
2018-11-09 17:06:36 -0300

    Atualização.

--- src/AVL/AVL.java
+++ src/AVL/AVL.java
@@ -1,30 +1,40 @@
 package AVL;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author FlowOverStack
  */
-public class AVL<K extends Comparable<K>, E> {
-    No<K, E> raiz;
+public class AVL<K extends Comparable<K>, V> {
+    No<K, V> raiz;
     int tamanho;
 
     public AVL() {
         this.tamanho = 0;
     }
     
-    public void inserir(K chave, E valor) {
+    public int altura(No<K, V> no) {
+        if (no == null) { 
+            return 0; 
+        }
+        return no.altura; 
+    }
+    
+    public void inserir(K chave, V valor) {
         if (raiz == null) {
             raiz = new No<>(chave, valor);
+            tamanho++;
         }else {
-            No<K, E> no = this.raiz;
+            No<K, V> no = this.raiz;
             boolean encontrou = false;
             do {
                 if (chave.compareTo(no.chave) > 0) {
                     if (no.direito == null) {  
                         no.direito = new No<>(chave, valor, no);
                         tamanho++;
-                        atualizarPesos(no, 1);
-                        balancear(no);
+                        no.direito.altura = Math.max(altura(no.esquerdo), altura(no.direito))+1;
+                        balancear(no.direito);
                         encontrou = true;
                     }else {
                         no = no.direito;
@@ -34,8 +44,8 @@ public class AVL<K extends Comparable<K>, E> {
                     if (no.esquerdo == null) {  
                         no.esquerdo = new No<>(chave, valor, no);
                         tamanho++;
-                        atualizarPesos(no, -1);
-                        balancear(no);
+                        no.esquerdo.altura = Math.max(altura(no.esquerdo), altura(no.direito))+1;
+                        balancear(no.esquerdo);
                         encontrou = true;
                     }else {
                         no = no.esquerdo;
@@ -48,21 +58,200 @@ public class AVL<K extends Comparable<K>, E> {
         }
     }
     
-    public void atualizarPesos(No<K, E> inicial, int peso) {
-        No<K, E> no = inicial;
-        while (no != raiz) {
-            no.peso += peso;
-            no = no.pai;
-        }
-        no.peso += peso;
+    public No<K, V> buscar(K chave) {
+        No<K, V> no = raiz;
+        boolean encontrou = false;
+        do {
+            if (no == null) {
+                encontrou = true;
+            } else if (chave.compareTo(no.chave) > 0) {
+                no = no.direito;
+            } else if (chave.compareTo(no.chave) < 0) {
+                no = no.esquerdo;
+            } else {
+                encontrou = true;
+            }
+        } while (!encontrou);
+        return no;
     }
     
-    public void balancear(No<K, E> inicial) {
-        No<K, E> no = inicial;
-        if (no.peso > 1) {
-            No<K, E> direito = no.direito;
-            
+    public V valorDe(K chave) {
+        No<K, V> no = raiz;
+        boolean encontrou = false;
+        do {
+            if (no == null) {
+                encontrou = true;
+            } else if (chave.compareTo(no.chave) > 0) {
+                no = no.direito;
+            } else if (chave.compareTo(no.chave) < 0) {
+                no = no.esquerdo;
+            } else {
+                encontrou = true;
+            }
+        } while (!encontrou);
+        return no.valor;
+    }
+    
+    public void remover(K chave) {
+        No<K, V> no = buscar(chave);
+        if (no != null) {
+            if (no.esquerdo == null && no.direito == null) {
+                if (no != raiz) {
+                    if (no.chave.compareTo(no.pai.chave) > 0) {
+                        no.pai.direito = null;
+
+                    } else {
+                        no.pai.esquerdo = null;
+                    }
+                    tamanho--;
+                } else {
+                    raiz = null;
+                    tamanho--;
+                }
+            } else if (no.direito != null && no.esquerdo == null) {
+                if (no != raiz) {
+                    if (no.chave.compareTo(no.pai.chave) > 0) {
+                        no.direito.pai = no.pai;
+                        no.pai.direito = no.direito;
+                    } else {
+                        no.direito.pai = no.pai;
+                        no.pai.esquerdo = no.direito;
+                    }
+                } else {
+                    no.direito.pai = null;
+                    raiz = no.direito;
+                }
+            } else if (no.direito == null && no.esquerdo != null) {
+                if (no != raiz) {
+                    if (no.chave.compareTo(no.pai.chave) > 0) {
+                        no.esquerdo.pai = no.pai;
+                        no.pai.direito = no.esquerdo;
+                    } else {
+                        no.esquerdo.pai = no.pai;
+                        no.pai.esquerdo = no.esquerdo;
+                    }
+                } else {
+                    no.esquerdo.pai = null;
+                    raiz = no.direito;
+                }
+            } else {
+                No<K, V> proximo = no.direito;
+                
+                while (proximo.esquerdo != null) {
+                    proximo = proximo.esquerdo;
+                }
+                
+                remover(proximo.chave);
+                no = buscar(no.chave);
+                
+                if (no == raiz) {
+                    raiz = proximo;
+                    proximo.pai = null;
+                    proximo.esquerdo = no.esquerdo;
+                    proximo.direito = no.direito;
+                    no.direito.pai = proximo;
+                    no.esquerdo.pai = proximo;
+                }else {
+                    proximo.pai = no.pai;
+                    proximo.esquerdo = no.esquerdo;
+                    proximo.direito = no.direito;
+                    no.direito.pai = proximo;
+                    no.esquerdo.pai = proximo;
+                }
+            }
         }
         
+        no.altura = Math.max(altura(no.esquerdo), altura(no.direito)) + 1; 
+        balancear(no);
+    }
+    
+    public int balanceamento(No<K, V> no) {
+        if (no == null) {
+            return 0;
+        }else {
+            return altura(no.esquerdo) - altura(no.direito); 
+        }
+    }
+    
+    public void balancear(No<K, V> inicial) {
+        No<K, V> no = inicial;
+        if (balanceamento(no) < -1) {
+            if (no.chave.compareTo(no.direito.chave) < 1) {
+                rotacaoEsquerda(no);
+            }else {
+                rotacaoDireita(no.direito);               
+                rotacaoEsquerda(no);
+            }
+        }else if (balanceamento(no) < -1) {
+            if (no.chave.compareTo(no.direito.chave) > 1) {
+                rotacaoDireita(no);
+            }else {
+                rotacaoEsquerda(no.esquerdo);
+                rotacaoDireita(no);
+            }
+        }
+        if (no.pai != null) {
+            balancear(no.pai);
+        }
+    }
+    
+    public void rotacaoEsquerda(No<K, V> pivo) {
+        No<K, V> filho = pivo.direito;
+        
+        if (pivo != raiz) {
+            if (pivo.pai.esquerdo == pivo) {
+                pivo.pai.esquerdo = filho;
+            }else {
+               pivo.pai.direito = filho;
+            }
+            filho.pai = pivo.pai;
+        }else {
+            filho.pai = null;
+            raiz = filho;
+        }
+        
+        pivo.pai = filho;
+        pivo.direito = filho.esquerdo;
+        filho.esquerdo = pivo;
+        
+        pivo.altura = Math.max(altura(pivo.esquerdo), altura(pivo.direito)) + 1; 
+        filho.altura = Math.max(altura(filho.esquerdo), altura(filho.direito)) + 1; 
+    }
+    
+    public void rotacaoDireita(No<K, V  > pivo) {
+        No<K, V> filho = pivo.esquerdo;
+        
+        if (pivo != raiz) {
+            if (pivo.pai.esquerdo == pivo) {
+                pivo.pai.esquerdo = filho;
+            }else {
+               pivo.pai.direito = filho;
+            }
+            filho.pai = pivo.pai;
+        }else {
+            filho.pai = null;
+            raiz = filho;
+        }
+        
+        pivo.pai = filho;
+        pivo.esquerdo = filho.direito;
+        filho.direito = pivo;
+        
+        pivo.altura = Math.max(altura(pivo.esquerdo), altura(pivo.direito)) + 1; 
+        filho.altura = Math.max(altura(filho.esquerdo), altura(filho.direito)) + 1; 
+    }
+    
+    ArrayList<No<K, V>> listarOrdem() {
+        ArrayList<No<K, V>> lista = new ArrayList<>();
+        listarOrdem(raiz, lista);
+        return lista;
+    }
+    
+    void listarOrdem(No<K, V> no, ArrayList<No<K, V>> lista) {
+        if (no != null) {
+            listarOrdem(no.esquerdo, lista);
+            lista.add(no);
+            listarOrdem(no.direito, lista);
+        }
     }
 }
--- src/AVL/No.java
+++ src/AVL/No.java
@@ -4,22 +4,22 @@ package AVL;
  *
  * @author FlowOverStack
  */
-public class No<K extends Comparable<K>, E> {
-    No<K, E> esquerdo;
-    No<K, E> direito;
-    No<K, E> pai;
+public class No<K extends Comparable<K>, V> {
+    No<K, V> esquerdo;
+    No<K, V> direito;
+    No<K, V> pai;
     K chave;
-    E valor;
-    int peso;
+    V valor;
+    int altura;
 
-    public No(K chave, E valor) {
-        this.peso = 0;
+    public No(K chave, V valor) {
+        this.altura = 1;
         this.chave = chave;
         this.valor = valor;
     }
     
-    public No(K chave, E valor, No<K, E> pai) {
-        this.peso = 0;
+    public No(K chave, V valor, No<K, V> pai) {
+        this.altura = 1;
         this.chave = chave;
         this.valor = valor;
         this.pai = pai;
